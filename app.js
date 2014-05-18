b8fd5041c64e7011860ca413ba8587c9327a0d12
Darren Burgess <darrentburgess@gmail.com>
2014-05-18 09:51:10 -0400

    added style.css.  added jqueryUI dialog.  completed JS

--- app.js
+++ app.js
@@ -3,10 +3,30 @@ $(document).ready(function() {
 
 var i;
 var text;
-var lineBreak = "<br/>";
+var lineBreak = " ";
 
-var fizzBuzz = function() {
-	for (i = 1; i <= 100; i++) {
+$('input').focus();
+
+$("#dialog").dialog({ autoOpen: false, modal: true });
+
+$('input').keypress(function(e){
+	var valNum = $(this).val();
+	if(e.which === 13){
+		if (valNum > 1000) {
+			console.log('test');
+			$('#dialog').dialog('open');
+			// alert(valNum + ' is too big.  You might crash your browser!');
+		} else {
+			$('p').html($(this).children());
+			fizzBuzz($(this).val());
+		}	
+		e.preventDefault();
+		$(this).focus().val('');
+	}
+});
+
+var fizzBuzz = function(theNum) {
+	for (i = 1; i <= theNum; i++) {
 		if (i % 15 === 0 ) {
 			text = "FizzBuzz";
 		} else if  (i % 3 === 0) {
@@ -20,10 +40,6 @@ var fizzBuzz = function() {
 		$('.result').append(text);
 	}
 };
-
-$('a').click(function() {
-	fizzBuzz();
-});
 
 //end jQuery 
 });
